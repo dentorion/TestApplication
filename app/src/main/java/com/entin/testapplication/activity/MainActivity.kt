8a34e20ccdadbc45099ca741984c0dc5bf583bc8
Denys <dentor2005@gmail.com>
2022-06-19 21:37:00 +0200

    Splash screen.

--- app/src/main/java/com/entin/testapplication/activity/MainActivity.kt
+++ app/src/main/java/com/entin/testapplication/activity/MainActivity.kt
@@ -2,11 +2,15 @@ package com.entin.testapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.entin.testapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }
