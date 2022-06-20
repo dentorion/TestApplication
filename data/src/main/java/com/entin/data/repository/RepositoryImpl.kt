7ec5fca9f3cee7e0f0a79d74045bf6c1b35b46ec
Denys <dentor2005@gmail.com>
2022-06-20 13:11:05 +0200

    Create Repository.

---
+++ data/src/main/java/com/entin/data/repository/RepositoryImpl.kt
@@ -0,0 +1,55 @@
+package com.entin.data.repository
+
+import com.entin.data.local.LocalDataSource
+import com.entin.data.remote.RemoteDataSource
+import com.entin.data.utils.mapToDomainModel
+import com.entin.data.utils.mapToRoomModel
+import com.entin.domain.model.UserDomain
+import com.entin.domain.repository.Repository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+import javax.inject.Inject
+
+class RepositoryImpl @Inject constructor(
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
+) : Repository {
+
+    private var firstResult = false
+    private var secondResult = false
+
+    override fun downloadDataFromAllApi(): Flow<Result<Boolean>> = flow {
+        remoteDataSource.downloadDataApiOne().onSuccess { responseOne ->
+            val usersFirstList = responseOne.map { apiOneResponse ->
+                apiOneResponse.mapToRoomModel()
+            }
+            localDataSource.saveAllUsers(usersFirstList)
+            firstResult = true
+        }
+
+        remoteDataSource.downloadDataApiTwo().onSuccess { responseTwo ->
+            val usersSecondList = responseTwo.map { apiTwoResponse ->
+                apiTwoResponse.mapToRoomModel()
+            }
+            localDataSource.saveAllUsers(usersSecondList)
+            secondResult = true
+        }
+
+        if (firstResult && secondResult) {
+            emit(Result.success(true))
+        } else {
+            emit(Result.failure(IOException()))
+        }
+    }
+
+    override fun getLocalSavedData(): Flow<Result<List<UserDomain>>> = flow {
+        emit(
+            Result.success(
+                localDataSource.getAllUsers().map { roomModel ->
+                    roomModel.mapToDomainModel()
+                }
+            )
+        )
+    }
+}
---
+++ domain/src/main/java/com/entin/domain/repository/Repository.kt
@@ -0,0 +1,16 @@
+package com.entin.domain.repository
+
+import com.entin.domain.model.UserDomain
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Repository interface
+ * Fetches data from RemoteDataSource
+ */
+
+interface Repository {
+
+    fun downloadDataFromAllApi(): Flow<Result<Boolean>>
+
+    fun getLocalSavedData(): Flow<Result<List<UserDomain>>>
+}
