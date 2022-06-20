1f98b136bb6df2f52cb3ce355246942401c50e5b
Denys <dentor2005@gmail.com>
2022-06-20 13:10:34 +0200

    Mapper for user entities.

---
+++ data/src/main/java/com/entin/data/utils/MapperUser.kt
@@ -0,0 +1,36 @@
+package com.entin.data.utils
+
+import com.entin.data.model.apiOne.ApiOneResponseItem
+import com.entin.data.model.apiTwo.ApiTwoResponse
+import com.entin.domain.model.UserDomain
+import com.entin.room.model.UserRoom
+
+/**
+ * Mapper
+ * CatJson converts into Domain Model to be shown for user.
+ */
+
+fun ApiOneResponseItem.mapToRoomModel(): UserRoom =
+    UserRoom(
+        nameUser = this.screenname,
+        avatarUrl = DEFAULT_AVATAR_URL,
+        source = API_ONE_NAME
+    )
+
+fun ApiTwoResponse.mapToRoomModel(): UserRoom =
+    UserRoom(
+        nameUser = this.login,
+        avatarUrl = this.avatar_url,
+        source = API_TWO_NAME,
+    )
+
+fun UserRoom.mapToDomainModel(): UserDomain =
+    UserDomain(
+        name = this.nameUser,
+        avatarUrl = this.avatarUrl,
+        source = this.source,
+    )
+
+const val DEFAULT_AVATAR_URL = "https://innostudio.de/fileuploader/images/default-avatar.png"
+const val API_ONE_NAME = "Dailymotion"
+const val API_TWO_NAME = "GitHub"
