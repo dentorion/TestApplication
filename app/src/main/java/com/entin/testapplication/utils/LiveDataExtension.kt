ceb56e376c2232e13d4b64eabd34f67a6da62f0d
Denys <dentor2005@gmail.com>
2022-06-20 16:21:20 +0200

    Add LiveDataExtension.

---
+++ app/src/main/java/com/entin/testapplication/utils/LiveDataExtension.kt
@@ -0,0 +1,13 @@
+package com.entin.testapplication.utils
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ * Wrapper under LiveData Observer
+ */
+
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>, observer: Observer<T>) {
+    liveData.observe(this, observer)
+}
